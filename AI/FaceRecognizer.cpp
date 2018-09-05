7d3f7baca8aaa491e05986c619b89923581d6404
dkulpa <1dawidk@gmail.com>
2018-09-05 09:12:05 +0200

    Camera init added

--- AI/FaceRecognizer.cpp
+++ AI/FaceRecognizer.cpp
@@ -10,7 +10,7 @@ void FaceRecognizer::run() {
 
     //Init camera
     camera= new Camera();
-    //camera->init(1024, 1024);
+    camera->init(1024, 1024);
 
     //Init dlib face detector
     face_detector= dlib::get_frontal_face_detector();
