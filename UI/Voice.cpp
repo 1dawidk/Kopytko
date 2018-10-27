72e57ad361a86383dcb8456bd725191fa4af3549
Dawid Kulpa <1dawidk@gmail.com>
2018-10-27 17:29:49 +0200

    no voice

--- UI/Voice.cpp
+++ UI/Voice.cpp
@@ -6,107 +6,107 @@ Voice::Voice(DataProcessor *dataProcessor) {
 }
 
 void Voice::init(string voicesDir) {
-    if( Mix_OpenAudio( 22050, MIX_DEFAULT_FORMAT, 2, 4096 ) == -1 ){
-        cout << "Error opening audio: " << Mix_GetError()  << endl;
-    }
-    this->voices_dir= voicesDir;
-    if(voices_dir[voicesDir.length()-1]!='/')
-        voices_dir+='/';
-
-    queueM= PTHREAD_MUTEX_INITIALIZER;
-    this->start();
+//    if( Mix_OpenAudio( 22050, MIX_DEFAULT_FORMAT, 2, 4096 ) == -1 ){
+//        cout << "Error opening audio: " << Mix_GetError()  << endl;
+//    }
+//    this->voices_dir= voicesDir;
+//    if(voices_dir[voicesDir.length()-1]!='/')
+//        voices_dir+='/';
+//
+//    queueM= PTHREAD_MUTEX_INITIALIZER;
+//    this->start();
 }
 
 void Voice::say(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-    char buff[512];
-    sprintf(buff, format, args);
-    string sentence(buff);
-
-    vector<Mix_Music*> *voiceSentence= new vector<Mix_Music*>();
-
-    size_t wcharPos=0;
-    size_t lastWcharPos=0;
-
-    while( lastWcharPos!=string::npos ){
-        wcharPos= sentence.find_first_of(' ', wcharPos+1);
-        string word;
-
-        if(wcharPos!=string::npos) {
-            if(lastWcharPos)
-                lastWcharPos++;
-            word = sentence.substr(lastWcharPos, wcharPos - lastWcharPos);
-        } else {
-            word = sentence.substr(lastWcharPos+1);
-        }
-
-        voiceSentence->push_back(loadWord(word));
-
-        lastWcharPos= wcharPos;
-    }
-
-    //Add to queue
-    pthread_mutex_lock(&queueM);
-    queue.push_back(voiceSentence);
-    pthread_mutex_unlock(&queueM);
+//    va_list args;
+//    va_start(args, format);
+//    char buff[512];
+//    sprintf(buff, format, args);
+//    string sentence(buff);
+//
+//    vector<Mix_Music*> *voiceSentence= new vector<Mix_Music*>();
+//
+//    size_t wcharPos=0;
+//    size_t lastWcharPos=0;
+//
+//    while( lastWcharPos!=string::npos ){
+//        wcharPos= sentence.find_first_of(' ', wcharPos+1);
+//        string word;
+//
+//        if(wcharPos!=string::npos) {
+//            if(lastWcharPos)
+//                lastWcharPos++;
+//            word = sentence.substr(lastWcharPos, wcharPos - lastWcharPos);
+//        } else {
+//            word = sentence.substr(lastWcharPos+1);
+//        }
+//
+//        voiceSentence->push_back(loadWord(word));
+//
+//        lastWcharPos= wcharPos;
+//    }
+//
+//    //Add to queue
+//    pthread_mutex_lock(&queueM);
+//    queue.push_back(voiceSentence);
+//    pthread_mutex_unlock(&queueM);
 }
 
 
 /************** PRIVATES ****************/
 void Voice::saySentence(vector<Mix_Music*> *voiceSentence) {
-    for(Mix_Music* m: *voiceSentence){
-        if( m != nullptr) {
-            Mix_PlayMusic(m, 1);
-            while (Mix_PlayingMusic());
-        }
-    }
+//    for(Mix_Music* m: *voiceSentence){
+//        if( m != nullptr) {
+//            Mix_PlayMusic(m, 1);
+//            while (Mix_PlayingMusic());
+//        }
+//    }
 }
 
 Mix_Music *Voice::loadWord(string word) {
-    string wordPath= dataProcessor->getDataPath()+voices_dir+word+".ogg";
-    Mix_Music *m= Mix_LoadMUS(wordPath.c_str());
-
-    return m;
+//    string wordPath= dataProcessor->getDataPath()+voices_dir+word+".ogg";
+//    Mix_Music *m= Mix_LoadMUS(wordPath.c_str());
+//
+//    return m;
 }
 
 void Voice::deleteSentence(vector<Mix_Music*> *voiceSentence) {
-    for(Mix_Music *m: *voiceSentence){
-        Mix_FreeMusic(m);
-    }
-
-    voiceSentence->clear();
+//    for(Mix_Music *m: *voiceSentence){
+//        Mix_FreeMusic(m);
+//    }
+//
+//    voiceSentence->clear();
 }
 
 void Voice::onStart() {
 }
 
 void Voice::onRun() {
-    if(!queue.empty()){
-        vector<Mix_Music*> *sentence;
-
-        //Get sentence sequence
-        pthread_mutex_lock(&queueM);
-        sentence= queue[0];
-        pthread_mutex_unlock(&queueM);
-
-        //Play sentence
-        saySentence(sentence);
-
-        //Remove sentence
-        deleteSentence(queue[0]);
-        delete queue[0];
-        pthread_mutex_lock(&queueM);
-        queue.erase(queue.begin());
-        pthread_mutex_unlock(&queueM);
-    }
+//    if(!queue.empty()){
+//        vector<Mix_Music*> *sentence;
+//
+//        //Get sentence sequence
+//        pthread_mutex_lock(&queueM);
+//        sentence= queue[0];
+//        pthread_mutex_unlock(&queueM);
+//
+//        //Play sentence
+//        saySentence(sentence);
+//
+//        //Remove sentence
+//        deleteSentence(queue[0]);
+//        delete queue[0];
+//        pthread_mutex_lock(&queueM);
+//        queue.erase(queue.begin());
+//        pthread_mutex_unlock(&queueM);
+//    }
 }
 
 void Voice::onStop() {
-    cout << "\t\tClean: Voice memory" << endl;
-    for(int i=0; i<queue.size(); i++){
-        deleteSentence(queue[0]);
-        delete queue[0];
-        queue.erase(queue.begin());
-    }
+//    cout << "\t\tClean: Voice memory" << endl;
+//    for(int i=0; i<queue.size(); i++){
+//        deleteSentence(queue[0]);
+//        delete queue[0];
+//        queue.erase(queue.begin());
+//    }
 }
